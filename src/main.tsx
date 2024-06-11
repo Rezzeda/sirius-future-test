8ac39af93f73fd1d529ac99a9ece99cc6e28a06a
Reseda <pro@pro.local>
2024-06-11 11:30:52 +0400

    prepare for deploy

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router basename='/sirius-future-test'>
         <App />
       </Router>
     </Provider>
